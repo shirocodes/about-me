36f160d6f0a371f2a138c20367d0f313be03b3a0
shirocodes <ishirapt@gmail.com>
2025-07-18 15:22:02 +0300

    Refactor(showcase section): Use GSAP and useRef hook to enable animations.

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import {useRef} from 'react'
+import { gsap } from 'gsap/gsap-core'
+import { ScrollTrigger } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+
 import project1 from '../assets/project1.jpg'
 import project2 from '../assets/project2.jpg'
 import project3 from '../assets/lp.png'
 
+
+gsap.registerPlugin(ScrollTrigger)
+
 const Showcase = () => {
+  const sectionRef = useRef(null)
+  const work1Ref = useRef(null)
+  const work2Ref = useRef(null)
+  const work3Ref = useRef(null)
+
+  useGSAP(() => {
+    const projects = [work1Ref.current, work2Ref.current, work3Ref.current]
+
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50, opacity: 0
+        },
+        {
+          y:0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: 'top bottom-=100'
+          }
+        }
+      )
+    })
+
+    gsap.fromTo(
+      sectionRef.current, 
+      {opacity: 0},
+      {opacity: 1, duration: 1.5}
+    )
+  }, [])
+
   return (
-    <div id='work' className='app-showcase'>
+    <section id='work' ref={sectionRef} className='app-showcase'>
       <div className='w-full'>
         <div className='showcaselayout'>
           {/* left project side */}
-          <div className='first-project-wrapper'>
+          <div className='first-project-wrapper' ref={work1Ref}>
             <div className='image-wrapper'>
               <img src={project3} alt='cancer predictive tool'/>
             </div>
@@ -22,13 +63,13 @@ const Showcase = () => {
 
           {/* Right projects */}
           <div className='project-list-wrapper overflow-hidden'>
-            <div className='project'>
+            <div className='project' ref={work2Ref}>
               <div className='image-wrapper bg-[#ffefdb]'>
                 <img src={project1} alt='A CLI-tool for cervical cancer prediction'/>
               </div>
               <h2>Predict cervical cancer risk, take control.</h2>
             </div>
-            <div className='project'>
+            <div className='project' ref={work3Ref}>
               <div className='image-wrapper bg-[#ffefdb]'>
                 <img src={project2} alt='An online meeting productivity coach'/>
               </div>
@@ -37,7 +78,7 @@ const Showcase = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
